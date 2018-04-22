b9a6edb12a92c70b7b123ac88f5e901fac95ff9b
swiftlyanerd <cesaramaral831@gmail.com>
2018-04-22 02:56:46 -0700

    detects if now playing, different strings if so

--- commands/lastfm.js
+++ commands/lastfm.js
@@ -18,16 +18,45 @@ module.exports = {
             user  : params[0]
         }, function (err, recentTracks) {
             if (err) {console.log(err);}
-            const embed = new Discord.RichEmbed()
-            .setColor(0xFF0000)
-            .setAuthor(params[0], `https://i.imgur.com/x5AhTlq.png`)
-            .addField("Current Song", `${recentTracks.track[0].name}`, true)
-            .addField("Current Artist", `${recentTracks.track[0].artist["#text"]}`, true)
-            .addField("Previous Song", `${recentTracks.track[1].name}`, true)
-            .addField("Previous Artist", `${recentTracks.track[0].artist["#text"]}`, true)
-            .setThumbnail(`${recentTracks.track[0].image[3]["#text"]}`);
+            try {
+                let nowPlaying = false;
+                let status = "";
+                try {
+                    if (recentTracks.track[0]["@attr"].nowplaying) {
+                        nowPlaying = true;
+                    }
+                } catch (undef) { // undefined
+                    console.log(undef);
+                }
 
-            message.channel.send({embed});
+                switch (nowPlaying) {
+                    case true:
+                        status = "Current";
+                        break;
+                    case false:
+                        status = "Most Recent";
+                        break;
+                }
+
+                const embed = new Discord.RichEmbed()
+                .setColor(0xFFFFFF)
+                .setAuthor(params[0], `https://i.imgur.com/x5AhTlq.png`)
+                .addField(`${status} Song`, `${recentTracks.track[0].name}`, true)
+                .addField(`${status} Artist`, `${recentTracks.track[0].artist["#text"]}`, true)
+                .addField("Previous Song", `${recentTracks.track[1].name}`, true)
+                .addField("Previous Artist", `${recentTracks.track[1].artist["#text"]}`, true)
+                .setThumbnail(`${recentTracks.track[0].image[3]["#text"]}`);
+                if(nowPlaying) {
+                    embed.setDescription("Currently Scrobbling");
+                } else {
+                    embed.setDescription(`Last scrobbled on: ${recentTracks.track[0].date["#text"]} (UTC)`);
+                }
+                message.channel.send({embed});
+        } catch (unf) { // user not found
+            console.log(unf);
+            message.reply(`${params[0]} is not a last.fm user. Please enter a valid last.fm username.`);
+        }
         });
+
     },
 };
